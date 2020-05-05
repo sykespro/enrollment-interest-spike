10ee2099d346b73b33a18154176191fab9acb43c
Daniel Sykes <sykespro@gmail.com>
2020-05-04 21:56:07 -0400

    added input model for submission and postman request

--- EnrollmentInterestSpike/OnEnrollInterestRecieved.cs
+++ EnrollmentInterestSpike/OnEnrollInterestRecieved.cs
@@ -14,22 +14,62 @@ namespace EnrollmentInterestSpike
     {
         [FunctionName("OnEnrollInterestRecieved")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post",
+            Route = null)] HttpRequest req,
+            [Queue("interest-submission")] IAsyncCollector<ResidentInterestInput> interestQueue,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
+            
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var submission = JsonConvert.DeserializeObject<ResidentInterestInput>(requestBody);
+            await interestQueue.AddAsync(submission);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = $"Interest Submission Recieved: {submission.Firstname} {submission.Lastname}";
 
             return new OkObjectResult(responseMessage);
         }
+    }
+
+    public class ResidentInterestInput
+    {
+        // facility information
+        public string Facility { get; set; }
+
+        // resident information
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string DateOfAdmission { get; set; } // date conversion
+        public string Gender { get; set; }
+        public string DOB { get; set; } // date formatted without time
+        public string SSN { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // resident in-facility information
+        public string Station { get; set; }
+        public string Room { get; set; }
+        public string Bed { get; set; }
+
+        // resident insurance information
+        public string MedicaidPlanNumber { get; set; }
+        public string MedicarePlanNumber { get; set; }
+        public string PrivateInsuranceCarrier { get; set; }
+        public string CardNumber { get; set; }
+        public string EffectiveDate { get; set; } // date formatted without time
+        public string InsuranceIDNumber { get; set; }
+        public string InsuranceGroupNumber { get; set; }
+        public string InsurancePhone { get; set; }
+        public string BIN { get; set; }
+        public string PCN { get; set; }
+
+        // responsible party (RP) information
+        public string Relationship { get; set; }
+        public string RPFirstname { get; set; } // refactor at some point
+        public string RPLastname { get; set; }
+        public string Address { get; set; }        
+        public string RPPhone { get; set; }
+        public string RPEmail { get; set; }
+
     }
 }
